4d5dea998e5fc5d464ffe06c0e748339b08a5b7a
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-11-08 15:27:15 +0300

    SUP, SWAP, INVALID, PC, getHeads modf

--- evm.go
+++ evm.go
@@ -13,6 +13,7 @@ LOOP:
 	for pc < len(code) {
 		opcode := code[pc]
 
+	SWITCH:
 		switch opcode {
 		case 00: // STOP
 			break LOOP
@@ -45,7 +46,7 @@ LOOP:
 			stack.s = append([]*big.Int{bn}, stack.s...)
 			pc += pb
 		case 0x50: // POP
-			_ = stack.getHead()
+			_ = stack.getHeads(1)[0]
 		case 0x01: // ADD
 			res := stack.oprHeads(new(big.Int).Add, false)
 			res.Mod(res, max.uint256Max)
@@ -85,7 +86,7 @@ LOOP:
 			}
 			stack.s = append([]*big.Int{res}, stack.s...)
 		case 0x10, 0x11: // LT, GT
-			heads := stack.getHeads()
+			heads := stack.getHeads(2)
 			var bn *big.Int
 
 			cmp := 1
@@ -100,7 +101,7 @@ LOOP:
 			}
 			stack.s = append([]*big.Int{bn}, stack.s...)
 		case 0x12, 0x13: // SLT, SGT
-			heads := stack.getHeads()
+			heads := stack.getHeads(2)
 			heads = twosComps(heads)
 			var bn *big.Int
 
@@ -116,7 +117,7 @@ LOOP:
 			}
 			stack.s = append([]*big.Int{bn}, stack.s...)
 		case 0x14: // EQ
-			heads := stack.getHeads()
+			heads := stack.getHeads(2)
 			var bn *big.Int
 			if heads[1].Cmp(heads[0]) == 0 {
 				bn = big.NewInt(1)
@@ -125,7 +126,7 @@ LOOP:
 			}
 			stack.s = append([]*big.Int{bn}, stack.s...)
 		case 0x15: // ISZERO
-			head := stack.getHead()
+			head := stack.getHeads(1)[0]
 			var bn *big.Int
 			bn = big.NewInt(0)
 
@@ -146,12 +147,29 @@ LOOP:
 			res := stack.oprHead(new(big.Int).Not, true)
 			stack.s = append([]*big.Int{res}, stack.s...)
 		case 0x1a: // BYTE
-			heads := stack.getHeads()
+			heads := stack.getHeads(2)
 			r := new(big.Int).Sub(big.NewInt(248), new(big.Int).Mul(heads[0], big.NewInt(8)))
 			res := new(big.Int).Rsh(heads[1], uint(r.Int64()))
 			bn := byteToBn("ff")
 			res = new(big.Int).And(res, bn)
 			stack.s = append([]*big.Int{res}, stack.s...)
+		case 0x80: // DUP1
+			head := stack.getHeads(1)[0]
+			stack.s = append([]*big.Int{head, head}, stack.s...)
+		case 0x82: // DUP3
+			heads := stack.getHeads(3)
+			stack.s = append([]*big.Int{heads[2], heads[0], heads[1], heads[2]}, stack.s...)
+		case 0x90: // SWAP1
+			heads := stack.getHeads(2)
+			stack.s = append([]*big.Int{heads[1], heads[0]}, stack.s...)
+		case 0x92: // SWAP3
+			heads := stack.getHeads(4)
+			stack.s = append([]*big.Int{heads[3], heads[1], heads[2], heads[0]}, stack.s...)
+		case 0xfe: // INVALID
+			break SWITCH
+		case 0x58: // PC
+			bn := big.NewInt(int64(pc))
+			stack.s = append([]*big.Int{bn}, stack.s...)
 		}
 		pc++
 	}
--- evm_operations.go
+++ evm_operations.go
@@ -4,20 +4,14 @@ import (
 	"math/big"
 )
 
-func (s *evmstack) getHeads() []*big.Int {
-	heads := []*big.Int{s.s[0], s.s[1]}
-	s.s = s.s[2:]
+func (s *evmstack) getHeads(n int) []*big.Int {
+	heads := s.s[0:n]
+	s.s = s.s[n:]
 	return heads
 }
 
-func (s *evmstack) getHead() *big.Int {
-	head := s.s[0]
-	s.s = s.s[1:]
-	return head
-}
-
 func (s *evmstack) oprHeads(f func(x *big.Int, y *big.Int) *big.Int, signed bool) *big.Int {
-	heads := s.getHeads()
+	heads := s.getHeads(2)
 	if signed {
 		heads = twosComps(heads)
 		res := f(heads[0], heads[1])
@@ -27,7 +21,7 @@ func (s *evmstack) oprHeads(f func(x *big.Int, y *big.Int) *big.Int, signed bool
 }
 
 func (s *evmstack) oprHead(f func(x *big.Int) *big.Int, signed bool) *big.Int {
-	head := s.getHead()
+	head := s.getHeads(1)[0]
 	if signed {
 		head = twosComp(head)
 		res := f(head)
